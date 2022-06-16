1a409f4032493489800072962182ceba26970514
alexv80 <42776667+alexv80@users.noreply.github.com>
2022-06-16 20:10:26 +0300

    Create variables.tf

---
+++ modules/workspace/variables.tf
@@ -0,0 +1,18 @@
+variable "workspace_settings" {
+  type = list(object({
+    workspace_name      = string
+    organization_name   = string
+    execution_mode      = string
+    agent_pool_id       = optional(string)
+    auto_apply          = string
+    speculative_enabled = bool
+    working_directory   = string
+    identifier          = string
+    ingress_submodules  = bool
+    oauth_token_id      = string
+    environment         = string
+    component           = string
+    branch              = string
+  }))
+  description = "Workspace set of settings"
+}
